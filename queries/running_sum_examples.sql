994dc80909ec344cb6b4e704a14eaf965a8f150e
divyarani-phondekar <divyaphondekar@gmail.com>
2024-11-12 11:10:44 +0530

    added query

--- queries/running_sum_examples.sql
+++ queries/running_sum_examples.sql
@@ -1,3 +1,5 @@
+-- 1. Get running sum of total
+
 SELECT invoiceid, total FROM Invoice i 
 
 InvoiceId|Total|
@@ -18,3 +20,34 @@ InvoiceId|Total|runningsum|
         3| 5.94|11.88|
         4| 8.91|20.79|
         5|13.86|34.65|
+
+------------------------------------------------------------------------------------
+-- 2. Get percentage of prize money
+
+CREATE TABLE proportion(name varchar, prize integer);
+
+INSERT INTO proportion values('Aakash', 100), ('Ajay', 200), ('Ajesh', 400),('Amey', 300)
+
+SELECT * FROM proportion p;
+
+name  |prize|
+------+-----+
+Aakash|  100|
+Ajay  |  200|
+Ajesh |  400|
+Amey  |  300|
+
+-- Query
+SELECT name, prize, total, (CAST(prize AS float)/total) * 100 
+FROM
+(SELECT name, prize, 
+sum(prize) over(ORDER BY name  ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS total
+FROM proportion p )
+
+-- Output
+name  |prize|total|(CAST(prize AS float)/total) * 100|
+------+-----+-----+----------------------------------+
+Aakash|  100| 1000|                              10.0|
+Ajay  |  200| 1000|                              20.0|
+Ajesh |  400| 1000|                              40.0|
+Amey  |  300| 1000|                              30.0|
